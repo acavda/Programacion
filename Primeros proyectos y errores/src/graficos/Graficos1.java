cb98a9f123ba8dd902bad6b0ba72edd94dca0d06
acavda <antoniocaniete@gmail.com>
2013-10-25 16:32:57 +0200

    PP

---
+++ Primeros proyectos y errores/src/graficos/Graficos1.java
@@ -0,0 +1,39 @@
+package graficos;
+
+import graphics.Canvas;
+import graphics.Color;
+import graphics.Rectangle;
+public class Graficos1 {
+
+	public static void main(String[] args) {
+		
+		
+		Canvas lienzo;
+		Rectangle rectangulo;
+		
+		lienzo = Canvas.getInstance();
+		
+		rectangulo = new Rectangle(10,10,50,100);
+		
+		
+	    rectangulo.draw();
+		
+		lienzo.pause();
+		
+		Canvas.pause();
+		
+		rectangulo.translate(250, 0);
+
+		rectangulo.setColor(Color.ORANGE);
+		rectangulo.fill();
+		
+		Canvas.pause();
+		
+		Color gris;
+		
+		gris= new Color(200,200,200);
+		
+		rectangulo.setColor(gris);
+	}
+
+}
---
+++ Primeros proyectos y errores/src/graficos/Semaforo.java
@@ -0,0 +1,85 @@
+package graficos;
+
+import graphics.Canvas;
+import graphics.Color;
+import graphics.Ellipse;
+import graphics.Rectangle;
+import graphics.Text;
+
+public class Semaforo {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		Canvas lienzo;
+		Ellipse circulo;
+		Rectangle rectangulo;
+		Text  texto;
+	
+		
+		texto = new Text(550,550,"Hola");
+		lienzo = Canvas.getInstance();
+		
+		rectangulo = new Rectangle(48,35,55,500);
+		
+		rectangulo.draw();
+		
+		rectangulo.setColor(Color.BLACK);
+		rectangulo.fill();
+		
+		
+		
+		Thread.sleep(1000);
+		
+		circulo = new Ellipse(50,50,50,50);
+		
+		circulo.draw();
+		
+		circulo.setColor(Color.RED);
+		circulo.fill();
+		
+		Thread.sleep(1000);
+		
+		circulo = new Ellipse(50,50,50,50);
+		
+		circulo.draw();
+		
+		circulo.setColor(Color.BLACK);
+		circulo.fill();
+		
+		
+		
+		Thread.sleep(1000);
+		
+		circulo = new Ellipse(50,150,50,50);
+		
+		circulo.draw();
+		circulo.setColor(Color.YELLOW);
+		circulo.fill();
+		
+		Thread.sleep(1000);
+		
+		circulo = new Ellipse(50,150,50,50);
+		
+		circulo.draw();
+		circulo.setColor(Color.BLACK);
+		circulo.fill();
+		
+		lienzo = Canvas.getInstance();
+		
+		Thread.sleep(1000);
+		
+		circulo = new Ellipse(50,250,50,50);
+		
+		circulo.draw();
+		circulo.setColor(Color.GREEN);
+		circulo.fill();
+		
+		
+	
+		
+		
+	 
+	}
+
+}
