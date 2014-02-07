2bf1475bb3dc4850f670201bd63253e522e736fb
acavda <antoniocaniete@gmail.com>
2014-02-07 16:47:52 +0100

    clases

--- UT5-CLASES/src/masejemplos/PruebaAgenda.java
+++ UT5-CLASES/src/masejemplos/PruebaAgenda.java
@@ -4,21 +4,14 @@ public class PruebaAgenda {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Contacto antonio;
-		
-		antonio= new Contacto("Antonio","cañete","655555555");
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+		antonio = new Contacto("Antonio", "cañete", "655555555");
+
+		System.out.println(antonio.getNombre());
+
+		System.out.println(antonio.getApellido1());
 
 	}
 
